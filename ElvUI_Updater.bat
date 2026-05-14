043a8a1a62ad74dcee5386545dd6157d50b496bc
Nijenna-tech <kevinschneider941@web.de>
2026-05-14 11:15:35 +0200

    Adjust timeout and update messages in updater script
    Updated timeout duration and messages for clarity.

--- ElvUI_Updater.bat
+++ ElvUI_Updater.bat
@@ -5,7 +5,7 @@ cd /d "%~dp0"
 :: --- HEADER (Wird nur einmal beim Start angezeigt) ---
 echo ======================================================================
 echo Nijenna ElvUI Updater: Wait for everything to be loaded...
-echo Checks for Update will start in 5 seconds.
+echo Checks for Update will start in 3 seconds.
 
 if not exist "Nijenna_Config.txt" (
     echo.
@@ -19,7 +19,7 @@ if not exist "Nijenna_Config.txt" (
 echo =======================================================================
 
 :: Initialer Timeout
-timeout /t 5 /nobreak
+timeout /t 3 /nobreak
 
 :START
 echo Checking internet connection...
@@ -29,15 +29,14 @@ ping 8.8.8.8 -n 1 -w 1000 >nul
 if %errorlevel% neq 0 (
     echo.
     echo [!] [%time:~0,8%] No internet connection detected. 
-    echo [!] Waiting 10 seconds before retrying...
-    timeout /t 10 /nobreak
+    echo [!] Waiting 3 seconds before retrying...
+    timeout /t 3 /nobreak
     echo.
     goto START
 )
 
 echo [+] Internet connection: OK!
-echo [+] Starting update in 3 seconds...
-timeout /t 3 /nobreak
+echo [+] Starting update
 
 :: Startet das PowerShell-Skript
 powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Nijenna_ElvUi_Updater.ps1"
@@ -59,7 +58,6 @@ if %errorlevel% equ 0 (
     exit
 )
 
-:: Falls ein unbekannter Fehler auftrat (Fenster zur Diagnose offen lassen)
 echo.
 echo [!] Der Updater wurde mit einem unerwarteten Status beendet (Code: %errorlevel%).
 pause
